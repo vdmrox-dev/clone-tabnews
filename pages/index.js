27e182fb2bffe5224024cb6aedfcce3071ea4069
Vinicius de Moraes <vdmrox@gmail.com>
2025-03-06 00:56:50 +0000

    create todayIs app

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,26 @@
-export default function Home() {
-    return <div>Test</div>
+import React, { useState, useEffect } from 'react'
+
+const CurrentDay = () => {
+  const [currentDay, setCurrentDay] = useState('')
+
+  useEffect(() => {
+    const daysOfWeek = [
+      'domingo',
+      'segunda-feira',
+      'terça-feira',
+      'quarta-feira',
+      'quinta-feira',
+      'sexta-feira',
+      'sábado',
+    ]
+    setCurrentDay(daysOfWeek[new Date().getDay()])
+  }, [])
+
+  return (
+    <div>
+      <h2>Oi meu benzinho, hoje é {currentDay || 'Loading...'}!</h2>
+    </div>
+  )
 }
+
+export default CurrentDay
